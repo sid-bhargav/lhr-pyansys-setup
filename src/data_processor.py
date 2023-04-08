b52c0beeea609add8a0bf6427a2865b473204991
Pranay Srivastava <31861735+sriv72@users.noreply.github.com>
2023-04-08 10:55:03 -0500

    new changes from previous weeks

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,14 +1,15 @@
 # DATA PROCESSOR FOR ALL INFO
 
 import yplus as yp
+import pandas as pd
 
 print(yp.calc(5, 9))
 
 
 class DataProcessor(object):
-    def __init__(self, design_table, named_selections):
-        self.dt = design_table
-        self.ns = named_selections
+    def __init__(self, design_table, named_selections): # treat inputs as file paths (with \\ for paths)
+        self.dt = pd.read_excel(design_table)
+        self.ns = pd.read_excel(named_selections)
 
     def preprocess(self):
         pass
--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-# data processor
+
+import json
 
 import data_processor as dp
 
-processor = dp.DataProcessor(None, None)
+# PREPROCESSING
+# IMPLEMENTATION 1: JSON FILE
+def preprocessing(filename):
+
+    with open(filename, 'r') as paths_json:
+        paths = json.load(paths_json)
+    
+    processor = dp.DataProcessor(paths["design_table"], paths["named_selections"])
+
 
 # mesh
 
 # cfd
+
+
+if __name__ == '__main__':
+    preprocessing('paths.json')
+    pass
